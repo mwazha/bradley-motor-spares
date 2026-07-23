295180aee78328f8dc536f6c11f1255ad4d604a1
Patrick Paradzayi <patrickparadzayi@gmail.com>
2026-07-23 11:21:25 +0200

    fix: update sitemap URL to include 'www' for consistency

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://bradleymotorspares.co.zw',
+      url: 'https://www.bradleymotorspares.co.zw',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
